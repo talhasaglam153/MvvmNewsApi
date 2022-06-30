f85919a8318388d330ac832b3039bcf4cb090c1e
Mustafa Talha SAGLAM <talhasaglam153@gmail.com>
2022-06-30 10:48:39 +0300

    initialized

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "195c7d91a77043708559221ad5f14cc7"
+        const val BASE_URL = "https://newsapi.org"
+
+    }
+
+}
